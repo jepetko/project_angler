df8e4ac863a1c1d8e2fb0b4b22c07f85932c8dc9
katarina <golbang.k@gmail.com>
2015-07-19 13:51:25 +0200

    add support for user stories

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,9 +20,42 @@
             .primaryPalette('green')
             .accentPalette('light-green');
     });
-    app.controller('ProjectController', [function() {
+    app.controller('ProjectController', ['$scope', function($scope) {
+        $scope.project = {
+            budget: '',
+            goLive: '',
+            description: '',
+            currentStory: {
+                asA: '',
+                iWant: '',
+                soThat: ''
+            },
+            stories: []
+        };
 
+        $scope.clearCurrentStory = function() {
+            $scope.project.currentStory = {
+                asA: '',
+                iWant: '',
+                soThat: ''
+            };
+        };
+
+        $scope.isCurrentStoryComplete = function() {
+            var story = $scope.project.currentStory;
+            console.info(story);
+            console.info(!!(story && story.asA && story.iWant && story.soThat));
+            return !!(story && story.asA && story.iWant && story.soThat);
+        };
+
+        $scope.addCurrentStory = function() {
+            if ($scope.isCurrentStoryComplete()) {
+                $scope.project.stories.push(angular.copy($scope.project.currentStory));
+            }
+            $scope.clearCurrentStory();
+        };
     }]);
+
     app.directive('budget', [function() {
         return {
             restrict: 'A',
@@ -85,17 +118,46 @@
             restrict: 'E',
             require: 'ngModel',
             replace: true,
-            template: '<textarea></textarea>',
+            scope: true,
+            template: '<div><textarea ng-model="story" columns="1" class="current-story"></textarea></div>',
             link: function(scope, el, attrs, ngModel) {
-                scope.parts = {
-                    asA: '',
-                    iWant: '',
-                    soThat: ''
-                }
-                el.on('keydown', function(evt) {
+                scope.story = '';
+
+                var parseUserInput = function() {
                     var regExp = new RegExp('^(As a(.*(?=I want to))?)(I want to(.*(?=So that I))?)?(So that I(.*)?)?\\.?$', 'gi');
-                    var groups = regExp.exec(ngModel.$modelValue);
+                    var groups = regExp.exec(scope.story);
                     console.info(groups);
+                    var updateModelArgs = [];
+                    if (groups) {
+                        if (groups.length > 2) {
+                            updateModelArgs[0] = groups[2];
+                            if (groups.length > 4) {
+                                updateModelArgs[1] = groups[4];
+                                if (groups.length > 6) {
+                                    updateModelArgs[2] = groups[6];
+                                }
+                            }
+                            updateModel.apply(null, updateModelArgs);
+                        }
+                    }
+                };
+
+                var updateModel = function(asA, iWant, soThat) {
+                    var value = {
+                        asA: asA,
+                        iWant: iWant,
+                        soThat: soThat
+                    }
+                    console.warn(value);
+                    ngModel.$setViewValue(value);
+                    ngModel.$setValidity('validStory', (asA && iWant && soThat));
+                    console.info(ngModel);
+                };
+
+                el.on('keydown', function(evt) {
+                    parseUserInput();
+                }).on('input', function(evt) {
+                    parseUserInput();
                 });
             }
         };
